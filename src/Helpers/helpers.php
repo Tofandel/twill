8a298c4db35b0643cd8b059e48a645be9278649c
Tofandel <adrien.foulon@tukan.hu>
2024-06-05 21:25:41 +0200

    Wrap classUsesDeep in once to improve performance

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -39,26 +39,28 @@ if (! function_exists('classUsesDeep')) {
      */
     function classUsesDeep($class, $autoload = true)
     {
-        $traits = [];
+        return once(function () use ($class, $autoload) {
+            $traits = [];
 
-        // Get traits of all parent classes
-        do {
-            $traits = array_merge(class_uses($class, $autoload), $traits);
-        } while ($class = get_parent_class($class));
+            // Get traits of all parent classes
+            do {
+                $traits = array_merge(class_uses($class, $autoload), $traits);
+            } while ($class = get_parent_class($class));
 
-        // Get traits of all parent traits
-        $traitsToSearch = $traits;
-        while (! empty($traitsToSearch)) {
-            $newTraits = class_uses(array_pop($traitsToSearch), $autoload);
-            $traits = array_merge($newTraits, $traits);
-            $traitsToSearch = array_merge($newTraits, $traitsToSearch);
-        }
+            // Get traits of all parent traits
+            $traitsToSearch = $traits;
+            while (! empty($traitsToSearch)) {
+                $newTraits = class_uses(array_pop($traitsToSearch), $autoload);
+                $traits = array_merge($newTraits, $traits);
+                $traitsToSearch = array_merge($newTraits, $traitsToSearch);
+            }
 
-        foreach (array_keys($traits) as $trait) {
-            $traits = array_merge(class_uses($trait, $autoload), $traits);
-        }
+            foreach (array_keys($traits) as $trait) {
+                $traits = array_merge(class_uses($trait, $autoload), $traits);
+            }
 
-        return array_unique($traits);
+            return array_unique($traits);
+        });
     }
 }
 
